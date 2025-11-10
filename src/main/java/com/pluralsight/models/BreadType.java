0186d7fba2e45faf51e3f6244f4f0d3c2f4f652a
Omer Faruk Akcam <hckmsnbdy@gmail.com>
2025-11-10 00:33:38 -0500

    Commit test

--- src/main/java/com/pluralsight/models/BreadType.java
+++ src/main/java/com/pluralsight/models/BreadType.java
@@ -3,6 +3,7 @@ package com.pluralsight.models;
     public final class BreadType {
         private BreadType() { /* no-op */ }
 
+
         public static final String WHITE = "white";
         public static final String WHEAT = "wheat";
         public static final String RYE   = "rye";
